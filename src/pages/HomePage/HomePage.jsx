e4679892a64ce68fb3ad709d8fde4740eb07592a
janicavc <crisostomo_janica@yahoo.com>
2023-08-21 23:48:03 -0700

    add message to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <h1>HomePage</h1>
+    <>
+      <h1>HomePage</h1>
+      <p>
+        <strong>Welcome to Workout Wiz - Your Fitness Companion!</strong><br /><br />
+        Are you ready to embark on a journey to a healthier, stroger you?<br />
+        Look no further! Workout Wiz is here to guide you every step of the way.<br />
+        🌟 Personalized Workouts: Our app tailors workouts to your fitness level and goals. Whether you're a beginner or a seasoned athlete, we've got the perfect plan for you.<br />
+        📈 Progress Monitoring: Watch your progress unfold with detailed stats and visualizations. Celebrate your victories and stay motivated.<br />
+        📱 Anywhere, Anytime: Workout Wiz is available on all your devices. No excuses – you can work out wherever you are.<br />
+        Ready to get started? Let's make fitness fun and achieve your goals together!<br />
+        Start your journey with Workout Wiz today and become the best version of yourself!<br />
+        <strong>🏆 Unlock Your Potential with Workout Wiz! 🏆</strong>
+      </p>
+    </>
   );
 }
