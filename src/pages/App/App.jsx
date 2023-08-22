3b89bd05ba32e870323407aac020abac2416d023
janicavc <crisostomo_janica@yahoo.com>
2023-08-22 15:16:32 -0700

    add profile page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -7,6 +7,7 @@ import NavBar from '../../components/NavBar/NavBar';
 import HomePage from '../HomePage/HomePage';
 import ExerciseCategoryPage from '../ExerciseCategoryPage/ExerciseCategoryPage';
 import CategoryDetailPage from '../CategoryDetailPage/CategoryDetailPage';
+import ProfilePage from '../ProfilePage/ProfilePage';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
@@ -21,6 +22,7 @@ export default function App() {
               <Route path='/' element={<HomePage />} />
               <Route path='/categories' element={<ExerciseCategoryPage />} />
               <Route path="/exercise/:categoryId" element={<CategoryDetailPage />} />
+              <Route path='/profile' element={<ProfilePage />} />
             </Routes>
           </>
           :
